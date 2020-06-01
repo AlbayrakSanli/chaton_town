13cee873b043aa0a08d1664308f545a945968a9e
Albayrak Furkan <albayrak.mo@hotmail.fr>
2020-06-01 23:07:51 +0200

    ok

---
+++ app/helpers/join_cart_item_helper.rb
@@ -0,0 +1,2 @@
+module JoinCartItemHelper
+end
---
+++ test/controllers/join_cart_item_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JoinCartItemControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
