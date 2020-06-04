13a416f89a899ce9157fd2a0aa0067177e498e6b
jeremyhallr <jeremy.h8@hotmail.fr>
2020-06-04 11:17:17 +0200

    Admin and User Mailers update

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: 'chaton-sauvage@yopmail.com'
+
+  def order_confirmation(order)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @order = order
+    @user = order.user
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Confirmation de votre commande !')
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  after_create :order_notification
+  after_create :order_notification, :order_confirmation
 
   belongs_to :user
   has_many :join_table_order_items
@@ -7,6 +7,10 @@ class Order < ApplicationRecord
 
   def order_notification
     AdminMailer.order_notification(self).deliver_now
+  end
+
+  def order_confirmation
+    UserMailer.order_confirmation(self).deliver_now
   end
 
 end
